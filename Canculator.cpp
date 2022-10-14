a8fb6e67b3ca05939eb974aa0bb99ae2da4f90ef
VasiliiSokolov <vasilysokolov@mail.ru>
2022-10-14 15:46:40 +0300

    Добавьте файлы проекта.

---
+++ Canculator.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+int main()
+{
+    double a, b, result;
+    char operation;
+    std::string buffer;
+    std::cout << "Enter your expression:\n";
+    std::cin >> buffer;
+    std::stringstream buffer_stream (buffer);
+    buffer_stream >> a >> operation >> b;
+
+    if (operation == '+')
+    {
+        result = a + b;
+    }
+    else if (operation == '-')
+    {
+        result = a - b;
+    }
+    else if (operation == '/')
+    {
+        result = a / b;
+    }
+    else if (operation == '*')
+    {
+        result = a * b;
+    }
+    else
+        std::cerr << "Wrong operation!" << std::endl;
+    std::cout << result << std::endl;
+}
